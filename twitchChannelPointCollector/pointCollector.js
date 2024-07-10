7de066ffff6c209ab130e04384404eeeda838cec
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-07-09 18:34:49 -0700

    Add files via upload

---
+++ twitchChannelPointCollector/pointCollector.js
@@ -0,0 +1,8 @@
+function collectChannelPoints(){
+  const button = document.querySelector("#live-page-chat > div > div > div.Layout-sc-1xcs6mc-0.iTiPMO.chat-shell.chat-shell__expanded > div > div > section > div > seventv-container > div > div:nth-child(2) > div.Layout-sc-1xcs6mc-0.eWfUfi.chat-input__buttons-container > div.Layout-sc-1xcs6mc-0.cNKHwD > div > div > div > div.Layout-sc-1xcs6mc-0.kxrhnx > div > div > div > button")
+  if(button) {
+    button.click()
+  }
+}
+
+collectChannelPoints();
